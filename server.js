17375da20c7ff5404be79ca4d9fe53925f3c21d8
claudiomss <claudiocld95@gmail.com>
2025-04-29 20:07:18 -0300

    teste

--- server.js
+++ server.js
@@ -317,8 +317,8 @@ const getCostACM = async (n) => {
 
 // 🔑 Substitua pelo caminho para o seu arquivo JSON da chave
 const auth = new google.auth.GoogleAuth({
-  keyFile: "/root/up/service.json",
-  // keyFile: "service.json",
+  // keyFile: "/root/up/service.json",
+  keyFile: "service.json",
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 })
 
@@ -840,24 +840,24 @@ async function enviarParaSheets(resultado) {
   }
 }
 
-// all_Camps()
+all_Camps()
 
-const cron = require("node-cron")
+// const cron = require("node-cron")
 
-// cron.schedule("0 * * * *", () => {
-cron.schedule("* * * * *", () => {
-  // cron.schedule("*/5 * * * *", () => {
-  all_Camps()
+// // cron.schedule("0 * * * *", () => {
+// cron.schedule("* * * * *", () => {
+//   // cron.schedule("*/5 * * * *", () => {
+//   all_Camps()
 
-  agora = new Date()
-  umaHoraAtras = new Date(agora.getTime() - 60 * 60 * 1000)
-  horaAtual = setData + "+" + formatHora(agora)
-  horaAnterior = setData + "+" + formatHora(umaHoraAtras)
-  horaRange = formatHora(umaHoraAtras) + "-" + formatHora(agora)
+//   agora = new Date()
+//   umaHoraAtras = new Date(agora.getTime() - 60 * 60 * 1000)
+//   horaAtual = setData + "+" + formatHora(agora)
+//   horaAnterior = setData + "+" + formatHora(umaHoraAtras)
+//   horaRange = formatHora(umaHoraAtras) + "-" + formatHora(agora)
 
-  camps = []
-  fullData = []
-  dados_cart = []
-  completo = []
-  conv = 0
-})
+//   camps = []
+//   fullData = []
+//   dados_cart = []
+//   completo = []
+//   conv = 0
+// })
